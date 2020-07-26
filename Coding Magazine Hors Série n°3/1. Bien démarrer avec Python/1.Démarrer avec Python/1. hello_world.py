4910696d5f507cff3eedf5e369ee348eafb2a590
meta <gael.lehchibi@outlook.com>
2020-07-26 20:12:32 +0200

    Nothing new

---
+++ Coding Magazine Hors Série n°3/1. Bien démarrer avec Python/1.Démarrer avec Python/1. hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# coding=utf-8
+
+import sys
+
+print("Hello world !")
+
+# Pas ajouté dans le magazine, je le rajoute pour vérifier la version de python exécutée.
+# Normalement, vous devriez avoir la version 2 de python de lancée.
+# Idem pour l'encodage. merci les américains
+print("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
+
