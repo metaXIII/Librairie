1c8f8bdffe609486d7437bba3c0f630a18d33b23
metaxiii <gael.lehchibi@outlook.com>
2020-08-27 18:18:51 +0200

    ajout des boutons de l'interface pour le jeu papier caillou ciseaux.

--- Coding Magazine Hors Série n°3/1. Bien démarrer avec Python/2. L'essentiel pour Python/4. Réalisez une interface graphique pour vos jeux Python/RockPaperScissor.py
+++ Coding Magazine Hors Série n°3/1. Bien démarrer avec Python/2. L'essentiel pour Python/4. Réalisez une interface graphique pour vos jeux Python/RockPaperScissor.py
@@ -2,79 +2,80 @@
 # -*- coding: utf-8 -*-
 
 import random
-import time
-
-rock = 1
-paper = 2
-scissor = 3
-
-names = {rock: "Rock", paper: "Paper", scissor: "Scissor"}
-rules = {rock: scissor, paper: rock, scissor: paper}
-
-player_score = 0
-computer_score = 0
+from Tkinter import *
+from ttk import *
 
 
-def start():
-    print("Let's play a game of paper, scissor, rock")
-    while game():
-        pass
-    scores()
+def gui():
+    rock = 1
+    paper = 2
+    scissor = 3
 
+    names = {rock: "Rock", paper: "Paper", scissor: "Scissor"}
+    rules = {rock: scissor, paper: rock, scissor: paper}
 
-def game():
-    player = move()
-    computer = random.randint(1, 3)
-    result(player, computer)
-    return play_again()
+    player_score = 0
+    computer_score = 0
 
-
-def move():
-    while True:
-        player = raw_input("Rock = 1\nPaper = 2\nScissor = 3`\nMake a move :)\n")
-        try:
-            player = int(player)
-            if player in (1, 2, 3):
-                return player
-        except ValueError:
+    def start():
+        while game():
             pass
-        print("Oops: I didn't understand that, please enter 1, 2 or 3\n")
 
+    def game():
+        player = player_choice.get()
+        computer = random.randint(1, 3)
+        computer_choice.set(names[computer])
+        result(player, computer)
 
-def result(player, computer):
-    print("1....")
-    time.sleep(1)
-    print("2.....")
-    time.sleep(1)
-    print("3 !")
-    time.sleep(0.5)
-    print("Computer throw {0} !".format(names[computer]))
-    global player_score, computer_score
-    if player == computer:
-        print("Tie Game")
-    else:
-        if rules[player] == computer:
-            print("Your victory has been assured")
-            player_score += 1
+    def result(player, computer):
+        if player == computer:
+            result_set.set("Match Nul ! ")
         else:
-            print("The computer laughs as you realise you've been defeated")
-            computer_score += 1
+            if rules[player] == computer:
+                result_set.set("Vous avez gagné ! ")
+                new_score = player_score.get()
+                new_score += 1
+                player_score.set(new_score)
+            else:
+                result_set.set("L'ordinateur rit car vous avez perdu ! ")
+                new_score = computer_score.get()
+                new_score += 1
+                computer_score.set(new_score)
 
+    rps_windows = Toplevel()
+    rps_windows.title("Pierre caillou ciseaux")
 
-def play_again():
-    answer = raw_input("Would you like to play again? y/n\n")
-    if answer in ("y", "Y", "yes", "Yes", "of course", "Of course"):
-        return answer
-    else:
-        print("Thank you very much for playing our game. See you next time")
+    player_choice = IntVar()
+    computer_choice = StringVar()
+    result_set = StringVar()
+    player_choice.set(1)
+    player_score = IntVar()
+    computer_score = IntVar()
 
+    rps_frame = Frame(rps_windows, padding='3 3 12 12', width=300)
+    rps_frame.grid(column=0, row=0, sticky=(N, W, E, S))
+    rps_frame.columnconfigure(0, weight=1)
+    rps_frame.rowconfigure(0, weight=1)
 
-def scores():
-    global player_score, computer_score
-    print("HIGH SCORES")
-    print "Player :", player_score
-    print "Computer :", computer_score
+    Label(rps_frame, text="Joueur").grid(column=1, row=1, sticky=W)
+
+    Radiobutton(rps_frame, text="Caillou", variable=player_choice, value=1).grid(column=1, row=2, sticky=W)
+    Radiobutton(rps_frame, text="Papier", variable=player_choice, value=2).grid(column=1, row=3, sticky=W)
+    Radiobutton(rps_frame, text="Ciseaux", variable=player_choice, value=3).grid(column=1, row=4, sticky=W)
+
+    Label(rps_frame, text="Ordinateur").grid(column=3, row=1, sticky=W)
+    Label(rps_frame, textvariable=computer_choice).grid(column=3, row=3, sticky=W)
+
+    Button(rps_frame, text="Tour", command=start).grid(column=2, row=2)
+
+    Label(rps_frame, text="Score").grid(column=1, row=5, sticky=W)
+    Label(rps_frame, textvariable=player_score).grid(column=1, row=6, sticky=W)
+
+    Label(rps_frame, text="Score").grid(column=3, row=5, sticky=W)
+    Label(rps_frame, textvariable=computer_score).grid(column=3, row=6, sticky=W)
+
+    Label(rps_frame, textvariable=result_set).grid(column=2, row=7)
 
 
 if __name__ == '__main__':
-    start()
+    gui()
