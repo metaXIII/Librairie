57ccec6e15c542c391ae981d8ea8f60c4206df6c
meta <gael.lehchibi@outlook.com>
2021-02-08 12:20:58 +0100

    ajout des fondamentaux python

---
+++ Code Academy 2017/1. Les fondamentaux/2. Encore plus de types de données Python/course.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+tm = open("./lib/timeMachine.txt", mode="r", encoding="utf-8")
+dict = {}
+for line in tm:
+	line = line.strip()
+	line = line.translate(str.maketrans("", "",'!"#«»$%&–—\'()*+,-./:;<=>?@[\\]^_`{|}~'))
+	line = line.lower()
+	line = line.split(' ')
+	for word in line:
+		if word in dict:
+			count = dict[word]
+			count += 1
+			dict[word] = count
+		else:
+			dict[word] = 1
+
+for word, count in dict.items():
+	print(word + " :" + str(count)) 
