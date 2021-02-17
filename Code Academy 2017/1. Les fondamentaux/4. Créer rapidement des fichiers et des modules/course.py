ec1143c185ce77000e06bfe503d3b464e3759ac1
metaxiii <lehchibi.gael@pm.me>
2021-02-17 16:44:36 +0100

    add and work in linux

--- Code Academy 2017/1. Les fondamentaux/4. Créer rapidement des fichiers et des modules/course.py
+++ Code Academy 2017/1. Les fondamentaux/4. Créer rapidement des fichiers et des modules/course.py
@@ -6,9 +6,10 @@ f = open("list.txt", "r")
 # tm = open("./lib/timeMachine.txt", mode="r", encoding="utf-8")
 
 def doYourWork():
+    print("\"début\"")
     for line in f:
-        print(line)
+        print(line.strip("\n"))
+    print("\"fin\"")
 
 if __name__ == "__main__":
-    print(doYourWork)
-
+    doYourWork()
