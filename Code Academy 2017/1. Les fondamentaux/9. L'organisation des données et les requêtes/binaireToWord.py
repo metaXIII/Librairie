45bfac5375d51ab0cdd89a8c8a57c3a2644e6095
metaxiii <lehchibi.gael@pm.me>
2021-02-27 19:22:32 +0100

    end convert binaire to string

--- Code Academy 2017/1. Les fondamentaux/9. L'organisation des données et les requêtes/binaireToWord.py
+++ Code Academy 2017/1. Les fondamentaux/9. L'organisation des données et les requêtes/binaireToWord.py
@@ -1,10 +1,9 @@
-import binascii
-
 def transition_to_string(value):
+    value = value.replace(" ", "")
     if len(value) < 4:
         print("Vous n'avez pas envoyé un ensemble binaire")
         return
-    if len(value) % 4 != 0 and len(value) % 8 != 0:
+    if len(value) % 4 != 0 or len(value) % 8 != 0:
         len_of_bin = len(value) % 8
         if len_of_bin != 0:
             how_many_bin_to_add = 8 - len_of_bin
@@ -17,15 +16,20 @@ def transition_to_string(value):
 
 
 def do_your_work(value):
-    print(value)
-    print(bin_to_number(value))
-    print(chr(bin_to_number(value)))
-    return
+    result = ""
+    for i in range(int(len(value) / 8)):
+        result += (bin_to_number(value[i * 8:((i + 1) * 8)]))
+    print(result)
 
 
 def bin_to_number(value):
-    return value[0:7]
+    result = 0
+    for i in range(len(value)):
+        char = value[i]
+        if char == "1":
+            result = result + pow(2, (8 - i) - 1)
+    return chr(result)
 
 
 if __name__ == '__main__':
-    transition_to_string("111000 01000010")
+    transition_to_string("   111000 01000010 00111000 01000010") # 8B8B # 8 -> 56 # B -> 66
