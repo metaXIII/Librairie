33ee31fbd6a039fe2bdb65c0684ee75478b5a6e4
meta <lehchibi.gael@pm.me>
2021-03-16 15:34:07 +0100

    Ajout de la partie Mariadb

---
+++ Code Academy 2017/2. Bases de données/2. MariaDB: pour bien commencer/readme.md
@@ -0,0 +1,52 @@
+# MariaDB
+
+## Installation MariaDB
+
+MariaDB est un fork de MySQL.
+Utiliser le package manager sous linux pour récupérer les sources.
+Installez ensuite la base de données sur votre poste avec la commande.
+
+
+```
+    sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+```
+
+Démarrez le service : 
+
+```
+    sudo systemctl start mysqld
+```
+
+N'hésitez pas à initialiser les services pour qu'ils démarrent directement au démarrage.
+
+```
+    sudo systemctl enable mysqld
+```
+
+
+## Se connecter
+
+```
+    sudo mysql -uroot
+```
+(par défaut, mysql n'a pas de mot de passe, vous pouvez ensuite modifier cet état en faisant quelques recherches)
+
+
+### Recommandations: 
+
+Pour travailler avec les bases de données, il est préférables d'attribuer les droits à un utilisateur pour une tache précise et donc ne pas utiliser root en permanence.
+
+
+# Dumps des bases de données :
+
+```
+    mysqldump [nom de la base de données] [nom de la table] -u root -p > save.sql
+```
+Cette commande permet de récupérer une table d'une base de données dans un fichier (save.sql ici)
+
+## Insertion depuis un fichier
+
+```
+    mysql [nom de la base de données] -u root -p < save.sql
+```
+Cette commande permet d'insérer depuis un fichier des données dans la base de données.
---
+++ Code Academy 2017/2. Bases de données/2. MariaDB: pour bien commencer/save.sql
@@ -0,0 +1,53 @@
+-- MariaDB dump 10.19  Distrib 10.5.9-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: lxfdata
+-- ------------------------------------------------------
+-- Server version	10.5.9-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `linuxes`
+--
+
+DROP TABLE IF EXISTS `linuxes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `linuxes` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `current_version` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `easy` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `linuxes`
+--
+
+LOCK TABLES `linuxes` WRITE;
+/*!40000 ALTER TABLE `linuxes` DISABLE KEYS */;
+INSERT INTO `linuxes` VALUES (1,'Red Star Linux','3.0',0),(2,'Arch Linux',NULL,0),(3,'Gentoo',NULL,1),(5,'Linux Mint','17.1 Rebecca',1),(6,'Debian','7.8 Wheezie',0),(7,'openSUSE','13.2',1),(8,'Fedora','21',0),(9,'Mageia','4.1',1),(10,'Elementary OS','0.2 Luna',1),(11,'Bohdi Linux','3.0.0',1);
+/*!40000 ALTER TABLE `linuxes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-16 15:16:43
