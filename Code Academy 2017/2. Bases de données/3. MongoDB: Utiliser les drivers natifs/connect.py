ddfe053a9ee8b7d2f139d24a1ae15ab5dc01b2a3
meta <lehchibi.gael@pm.me>
2021-03-24 15:52:19 +0100

    restore

--- Code Academy 2017/2. Bases de données/3. MongoDB: Utiliser les drivers natifs/connect.py
+++ Code Academy 2017/2. Bases de données/3. MongoDB: Utiliser les drivers natifs/connect.py
@@ -4,12 +4,10 @@ from pymongo import MongoClient
 
 client = MongoClient()
 client = MongoClient("127.0.0.1", 27017)
+
 db = client.LXF
-
 sampleData = db.sampleData
-
 item = sampleData.find_one()
 
 print(item['_id'])
 print(item['x'])
-
