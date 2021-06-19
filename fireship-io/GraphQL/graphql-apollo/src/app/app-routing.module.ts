bfc05213ff68f23293ebe09632a5316b1e4d9f23
Meta <lehchibi.gael@pm.me>
2021-06-19 16:53:28 +0200

    add GraphQL project

---
+++ fireship-io/GraphQL/graphql-apollo/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LaunchDetailsComponent } from './launch-details/launch-details.component';
+import { LaunchListComponent } from './launch-list/launch-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LaunchListComponent
+  },
+  {
+    path: ':id',
+    component: LaunchDetailsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ fireship-io/GraphQL/graphql-apollo/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LaunchListComponent } from './launch-list/launch-list.component';
+import { LaunchDetailsComponent } from './launch-details/launch-details.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LaunchListComponent,
+    LaunchDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
