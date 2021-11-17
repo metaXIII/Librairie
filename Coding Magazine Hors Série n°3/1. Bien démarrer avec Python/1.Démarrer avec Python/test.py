0e3d925e3a51c86a48c670eaa3520ecd51c4c007
gael LEHCHIBI <gael.lehchibi@vialink.fr>
2021-11-17 11:39:14 +0100

    remove ununcessary comment

--- Coding Magazine Hors Série n°3/1. Bien démarrer avec Python/1.Démarrer avec Python/test.py
+++ Coding Magazine Hors Série n°3/1. Bien démarrer avec Python/1.Démarrer avec Python/test.py
@@ -7,9 +7,6 @@ import datetime
 begin_time = datetime.datetime.now()
 print("Hello world !")
 
-# Pas ajouté dans le magazine, je le rajoute pour vérifier la version de python exécutée.
-# Normalement, vous devriez avoir la version 2 de python de lancée.
-# Idem pour l'encodage. merci les américains
 i = 0
 while (i < 1000000):
     print(i)
